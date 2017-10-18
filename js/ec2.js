481318e1bdff7609f9732232443d9dd28c29f552
mihaioltean <moltean@cs.ubbcluj.ro>
2017-10-18 18:25:38 +0300

    am implementat mai obiectual

--- js/ec2.js
+++ js/ec2.js
@@ -1,40 +1,38 @@
-function citeste()
+
+function ecuatie2()
 {
-	var val_a = document.getElementById("a").value;
-	var val_b = document.getElementById("b").value;
-	var val_c = document.getElementById("c").value;
-	
-	var coef = {a:val_a, b:val_b, c:val_c};
-	
-	return coef;
-}
 //-------------------------------------
-function rezolva_ec2(coef)
-{
-	var delta = coef.b * coef.b - 4 * coef.a * coef.c;
-	var sol_x1, sol_x2;
+	this.citeste = function(){
+		this.a = document.getElementById("a").value;
+		this.b = document.getElementById("b").value;
+		this.c = document.getElementById("c").value;
+	};
+//-------------------------------------
+	this.calculeaza = function(){
+		var delta = this.b * this.b - 4 * this.a * this.c;
+	
 	if (delta >= 0){
-		sol_x1 = {re:(-coef.b + Math.sqrt(delta)) / (2 * coef.a), im:0}
-		sol_x2 = {re:(-coef.b - Math.sqrt(delta)) / (2 * coef.a), im:0};
+		this.x1 = {re:(-this.b + Math.sqrt(delta)) / (2 * this.a), im:0}
+		this.x2 = {re:(-this.b - Math.sqrt(delta)) / (2 * this.a), im:0};
 	}
 	else{
-		sol_x1 = {re:-coef.b / (2 * coef.a), im: Math.sqrt(-delta) / (2 * coef.a)};
-		sol_x2 = {re:-coef.b / (2 * coef.a), im:-Math.sqrt(-delta) / (2 * coef.a)};
+		this.x1 = {re:-this.b / (2 * this.a), im: Math.sqrt(-delta) / (2 * this.a)};
+		this.x2 = {re:-this.b / (2 * this.a), im:-Math.sqrt(-delta) / (2 * this.a)};
 	}
-	var solutii = {x1:sol_x1, x2:sol_x2};
-	return solutii;
-}
+	};
 //-------------------------------------
-function afiseaza_solutie(x, id)
-{
-	document.getElementById(id).innerHTML = x.re + "+" + x.im + "i";
+	this.afiseaza = function(){
+		document.getElementById("x1").innerHTML = this.x1.re + "+" + this.x1.im + "i";
+		document.getElementById("x2").innerHTML = this.x2.re + "+" + this.x2.im + "i";
+	};
+//-------------------------------------
 }
 //-------------------------------------
 function rezolva()
 {
-	var coef = citeste();
-	var solutii = rezolva_ec2(coef);
-	afiseaza_solutie(solutii.x1, "x1");
-	afiseaza_solutie(solutii.x2, "x2");
+	var e = new ecuatie2();
+	e.citeste();
+	e.calculeaza();
+	e.afiseaza();
 }
 //-------------------------------------
