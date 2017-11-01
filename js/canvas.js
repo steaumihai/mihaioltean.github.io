a3b09f16ee1e8673b5e69f1b3bf244728375f8cf
mihaioltean <mihai.oltean@gmail.com>
2017-11-01 18:54:35 +0200

    am schimbat culoarea

--- js/canvas.js
+++ js/canvas.js
@@ -1,4 +1,4 @@
- document.getElementById("id_logic_level_version").innerHTML = "Logic level version: 2017.11.01.2"; 
+ document.getElementById("id_logic_level_version").innerHTML = "Logic level version: 2017.11.01.3"; 
  
  var canvas = document.getElementById("id_canvas");
  var context = canvas.getContext("2d");
@@ -8,5 +8,5 @@
  var h = canvas.getAttribute("height");
  
  context.arc(w / 2, h / 2, 10, 0, 2 * Math.PI);
- 
+ context.strokeStyle = "red";
  context.stroke();
