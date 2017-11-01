e7228bb3782f869eceba500375cef18bbfe13a36
mihaioltean <mihai.oltean@gmail.com>
2017-11-01 18:29:46 +0200

    am sters suprafata

--- js/sensors.js
+++ js/sensors.js
@@ -14,6 +14,8 @@ function deseneaza_cerc(gamma, beta)
 	var w = canvas.getAttribute("width");
 	var h = canvas.getAttribute("height");
 	
+	context.clearRect(0, 0, w, h);
+	
 	var centru = {x : w / 2, y : h / 2};
 	var raza = 10;
 	var max_deplasare_x = w / 2 - raza;
