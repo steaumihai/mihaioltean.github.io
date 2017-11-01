75978bae753840311f1c9270d9e7a12520afdcc0
mihaioltean <mihai.oltean@gmail.com>
2017-11-01 17:42:20 +0200

    exemplu grafica canvas

---
+++ js/canvas.js
@@ -0,0 +1,10 @@
+ document.getElementById("id_logic_level_version").innerHTML = "Logic level version: 2017.11.01.0"; 
+ 
+ var canvas = document.getElementById("id_canvas");
+ var context = canvas.getContext("2d");
+ 
+ context.beginPath();
+ context.moveTo(10, 10);
+ context.lineTo(100, 200);
+ context.lineTo(200, 100);
+ context.stroke();
