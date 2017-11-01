d8eab6a73d88ae19759d7015ca84e73620e56a9b
mihaioltean <mihai.oltean@gmail.com>
2017-11-01 17:45:35 +0200

    close path

--- js/canvas.js
+++ js/canvas.js
@@ -1,4 +1,4 @@
- document.getElementById("id_logic_level_version").innerHTML = "Logic level version: 2017.11.01.0"; 
+ document.getElementById("id_logic_level_version").innerHTML = "Logic level version: 2017.11.01.1"; 
  
  var canvas = document.getElementById("id_canvas");
  var context = canvas.getContext("2d");
@@ -7,4 +7,5 @@
  context.moveTo(10, 10);
  context.lineTo(100, 200);
  context.lineTo(200, 100);
+ context.closePath();
  context.stroke();
