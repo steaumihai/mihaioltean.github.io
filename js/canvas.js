f384d15c0779c619262c263113e4a6eefb7755b8
mihaioltean <mihai.oltean@gmail.com>
2017-11-01 17:54:49 +0200

    desenare cerc

--- js/canvas.js
+++ js/canvas.js
@@ -1,11 +1,12 @@
- document.getElementById("id_logic_level_version").innerHTML = "Logic level version: 2017.11.01.1"; 
+ document.getElementById("id_logic_level_version").innerHTML = "Logic level version: 2017.11.01.2"; 
  
  var canvas = document.getElementById("id_canvas");
  var context = canvas.getContext("2d");
  
  context.beginPath();
- context.moveTo(10, 10);
- context.lineTo(100, 200);
- context.lineTo(200, 100);
- context.closePath();
+ var w = canvas.getAttribute("width");
+ var h = canvas.getAttribute("height");
+ 
+ context.arc(w / 2, h / 2, 10, 0, 2 * Math.PI);
+ 
  context.stroke();
