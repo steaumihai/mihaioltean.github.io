4452a87bb1cf8580515cc5f49b5eeb67d5dc18e7
mihaioltean <mihai.oltean@gmail.com>
2017-11-01 19:19:35 +0200

    desenare patrat

--- js/sensors.js
+++ js/sensors.js
@@ -1,5 +1,5 @@
 
-document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.01.3"; 
+document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.01.4"; 
 window.addEventListener('deviceorientation', ondeviceorientation);
 window.addEventListener('devicemotion', ondevicemotion);
 
@@ -45,6 +45,30 @@ function deseneaza_cerc_canvas(gamma, beta)
 	context.stroke();
 }
 //----------------------------------------
+function deseneaza_patrat_canvas(alpha, gamma, beta)
+{
+	var canvas = document.getElementById("id_canvas");
+	var context = canvas.getContext("2d");
+ 
+	context.beginPath();
+	var w = canvas.getAttribute("width");
+	var h = canvas.getAttribute("height");
+	
+	context.clearRect(0, 0, w, h);
+	
+	var centru = {x : w / 2, y : h / 2};
+	var latura = 10;
+	var max_deplasare_x = w / 2 - raza;
+	var max_deplasare_y = h / 2 - raza;
+ // gamma == 0 -> centru.x
+ // gamma == -90 -> 0 + raza
+ // gamma == 90 -> w - raza
+	
+	//context.arc(centru.x + gamma / 90 * max_deplasare_x, centru.y + beta / 90 * max_deplasare_y, raza, 0, 2 * Math.PI);
+	context.strokeRect(centru.x - latura / 2 + gamma / 90 * max_deplasare_x, centru.y - latura / 2 + beta / 90 * max_deplasare_y, latura, latura);
+	context.stroke();
+}
+//----------------------------------------
 function ondeviceorientation(event) 
 {
 	var alpha = event.alpha;
@@ -56,7 +80,8 @@ function ondeviceorientation(event)
 	document.getElementById("id_gamma").innerHTML = "gamma = " + Math.round(gamma * 10) / 10;
 	
 	//deseneaza_cerc_canvas(gamma, beta);
-	deseneaza_cerc_svg(gamma, beta);
+	// deseneaza_cerc_svg(gamma, beta);
+	deseneaza_patrat_canvas(alpha, gamma, beta);
 }
 //----------------------------------------
 function ondevicemotion(event) 
