dda8e7db9ce82298a620bc8bcf53437fd1ce2e78
mihaioltean <mihai.oltean@gmail.com>
2017-11-08 17:37:38 +0200

    corectat bug

--- js/touch.js
+++ js/touch.js
@@ -1,9 +1,9 @@
- document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.08.0"; 
+ document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.08.1"; 
  
  var canvas = document.getElementById("id_canvas");
  var context = canvas.getContext("2d");
 
- windows.addEventListener("touchstart", on_touch_start);
+ window.addEventListener("touchstart", on_touch_start);
  
  
  function on_touch_start(e)
