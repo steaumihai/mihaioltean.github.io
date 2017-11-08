0fbe80ec152e3e42b8700fbe7322710b0687e870
mihaioltean <mihai.oltean@gmail.com>
2017-11-08 17:50:39 +0200

    canvas touch start

--- js/touch.js
+++ js/touch.js
@@ -1,9 +1,9 @@
- document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.08.1"; 
+ document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.08.2"; 
  
  var canvas = document.getElementById("id_canvas");
  var context = canvas.getContext("2d");
 
- window.addEventListener("touchstart", on_touch_start);
+ canvas.addEventListener("touchstart", on_touch_start);
  
  
  function on_touch_start(e)
@@ -12,7 +12,7 @@
 	 var touches = e.changedTouches;
 	 for (i = 0; i < touches.length; i++){
 		 context.beginPath();
-		 context.arc(touches[i].pageX, touches[i].pageY, 10, 0, Math.PI);
+		 context.arc(touches[i].pageX, touches[i].pageY, 10, 0, 2 * Math.PI);
 		 context.stroke();
 	 }
  }
