598426fd87ed1cda157bf26a377e5e4d69ad7ff0
mihaioltean <mihai.oltean@gmail.com>
2017-11-08 18:45:17 +0200

    touch move

--- js/touch.js
+++ js/touch.js
@@ -1,9 +1,13 @@
- document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.08.4"; 
+ document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.08.5"; 
  
  var canvas = document.getElementById("id_canvas");
  var context = canvas.getContext("2d");
+ var rect = canvas.getBoundingClientRect();
 
  canvas.addEventListener("touchstart", on_touch_start);
+  canvas.addEventListener("touchmove", on_touch_move);
+  
+  var touch_id = [];
  //-----------------------------------
  function generate_random_color()
  {
@@ -14,15 +18,37 @@
 	 return color;
  }
  //-----------------------------------
- function on_touch_start(e)
- {
-	 var rect = canvas.getBoundingClientRect();
+function on_touch_start(e)
+{
 	 var touches = e.changedTouches;
 	 for (i = 0; i < touches.length; i++){
+		touch_id.push({id:touches[i].identifier, color:generate_random_color()});
+		 
 		 context.beginPath();
 		 context.arc(touches[i].pageX - rect.left, touches[i].pageY - rect.top, 10, 0, 2 * Math.PI);
-		 context.strokeStyle = generate_random_color();
+		 context.strokeStyle = touch_id[touch_id.length - 1].color;
+		 context.fillStyle = touch_id[touch_id.length - 1].color;
+		 context.fill();
 		 context.stroke();
 	 }
  }
- //-----------------------------------
+//-----------------------------------
+function on_touch_move(e)
+{
+	var touches = e.changedTouches;	
+	for (i = 0; i < touches.length; i++){
+		var color = "#FFFFFF";
+		for (j = 0; j < touch_id.length; j++)
+			if (touches[i].identifier == touch_id[j].id){
+				color = touch_id[j].color;
+				break;
+			}
+		 context.beginPath();
+		 context.arc(touches[i].pageX - rect.left, touches[i].pageY - rect.top, 10, 0, 2 * Math.PI);
+		 context.strokeStyle = color;
+		 context.fillStyle = color;
+		 context.fill();
+		 context.stroke();
+	}
+}
+//-----------------------------------
