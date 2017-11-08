515fcb74d5712be845d3d5ce36684e17bdedec57
mihaioltean <mihai.oltean@gmail.com>
2017-11-08 18:15:43 +0200

    culoare aleatoare

--- js/touch.js
+++ js/touch.js
@@ -1,11 +1,19 @@
- document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.08.3"; 
+ document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.08.4"; 
  
  var canvas = document.getElementById("id_canvas");
  var context = canvas.getContext("2d");
 
  canvas.addEventListener("touchstart", on_touch_start);
- 
- 
+ //-----------------------------------
+ function generate_random_color()
+ {
+	 var litere = "0123456789ABCDEF";
+	 var color = "#";
+	 for (i = 0; i < 6; i++)
+		 color += litere[Math.floor(Math.random() * 16)];
+	 return color;
+ }
+ //-----------------------------------
  function on_touch_start(e)
  {
 	 var rect = canvas.getBoundingClientRect();
@@ -13,6 +21,8 @@
 	 for (i = 0; i < touches.length; i++){
 		 context.beginPath();
 		 context.arc(touches[i].pageX - rect.left, touches[i].pageY - rect.top, 10, 0, 2 * Math.PI);
+		 context.strokeStyle = generate_random_color();
 		 context.stroke();
 	 }
  }
+ //-----------------------------------
