351efbe8aa38001d2bd69afc1acdd58c7c504175
mihaioltean <mihai.oltean@gmail.com>
2017-11-08 19:09:33 +0200

    prevent default

--- js/touch.js
+++ js/touch.js
@@ -1,4 +1,4 @@
- document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.08.7"; 
+ document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.08.8"; 
  
  var canvas = document.getElementById("id_canvas");
  var context = canvas.getContext("2d");
@@ -20,6 +20,7 @@
  //-----------------------------------
 function on_touch_start(e)
 {
+	e.preventDefault();
 	 var touches = e.changedTouches;
 	 for (var i = 0; i < touches.length; i++){
 		touch_id.push({id:touches[i].identifier, color:generate_random_color()});
@@ -35,6 +36,7 @@ function on_touch_start(e)
 //-----------------------------------
 function on_touch_move(e)
 {
+	e.preventDefault();
 	var touches = e.changedTouches;	
 	for (var i = 0; i < touches.length; i++){
 		var color = "#FFFFFF";
