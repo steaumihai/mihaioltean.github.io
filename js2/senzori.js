13552f0430ad00a68520ae73854462e9067539af
mihaioltean <mihai.oltean@gmail.com>
2017-11-13 19:39:50 +0200

    am inceput devicemotion

--- js2/senzori.js
+++ js2/senzori.js
@@ -1,9 +1,16 @@
 
 window.addEventListener("deviceorientation", on_device_orientation);
-
+window.addEventListener("devicemotion", on_device_motion);
+//--------------------------------
 function on_device_orientation(e)
 {
 	document.getElementById("id_z").innerHTML = "z=" + e.alpha;
 	document.getElementById("id_x").innerHTML = "x=" + e.beta;
 	document.getElementById("id_y").innerHTML = "y=" + e.gamma;
 }
+//--------------------------------
+function on_device_motion(e)
+{
+
+}
+//--------------------------------
