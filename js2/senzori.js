bfe1b5e09efb355190f5e2d056b0ae2e305534b6
mihaioltean <mihai.oltean@gmail.com>
2017-11-13 19:12:39 +0200

    am citit senzorii de orientare

--- js2/senzori.js
+++ js2/senzori.js
@@ -1,1 +1,9 @@
-document.getElementById("id_x").innerHTML = "salut";
+
+window.addEventListener("deviceorientation", on_device_orientation);
+
+function on_device_orientation(e)
+{
+	document.getElementById("id_z").innerHTML = e.alpha;
+	document.getElementById("id_x").innerHTML = e.beta;
+	document.getElementById("id_y").innerHTML = e.gamma;
+}
