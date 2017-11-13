96a9058cf0c61037e5a51571266a04b9427bdd00
mihaioltean <mihai.oltean@gmail.com>
2017-11-13 19:26:20 +0200

    am adaugat sir

--- js2/senzori.js
+++ js2/senzori.js
@@ -3,7 +3,7 @@ window.addEventListener("deviceorientation", on_device_orientation);
 
 function on_device_orientation(e)
 {
-	document.getElementById("id_z").innerHTML = e.alpha;
-	document.getElementById("id_x").innerHTML = e.beta;
-	document.getElementById("id_y").innerHTML = e.gamma;
+	document.getElementById("id_z").innerHTML = "z=" + e.alpha;
+	document.getElementById("id_x").innerHTML = "x=" + e.beta;
+	document.getElementById("id_y").innerHTML = "y=" + e.gamma;
 }
