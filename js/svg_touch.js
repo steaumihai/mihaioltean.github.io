b4704dc3ec473d9b7c6dcc7b3a5e8cfb4a3600f9
mihaioltean <mihai.oltean@gmail.com>
2017-11-15 18:05:40 +0200

    update business level

--- js/svg_touch.js
+++ js/svg_touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.15.1"; 
+document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.15.2"; 
 
 addEventListener("touchstart", on_touch_start);
 
