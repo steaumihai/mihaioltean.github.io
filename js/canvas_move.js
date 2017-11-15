8a7a18d8860cf1a355d680c81d1572a767912567
mihaioltean <mihai.oltean@gmail.com>
2017-11-15 19:41:17 +0200

    onload image

--- js/canvas_move.js
+++ js/canvas_move.js
@@ -1,13 +1,13 @@
-document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.15.1"; 
+document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.15.2"; 
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
 var rect_canvas = canvas.getBoundingClientRect();
 var img = document.getElementById("id_img");
-
-context.drawImage(img, 100, 100, 100, 100);
-var rect_img = img.getBoundingClientRect();
-
+img.onload = function(){
+	context.drawImage(img, 100, 100, 100, 100);
+	var rect_img = img.getBoundingClientRect();
+}
 canvas.addEventListener("touchmove", on_touch_move);
 
 //----------------------------------------
