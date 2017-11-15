c95ab1f5827e498c413cf145c5292bf1d422f918
mihaioltean <mihai.oltean@gmail.com>
2017-11-15 19:12:36 +0200

    circle inside box

--- js/svg_move.js
+++ js/svg_move.js
@@ -1,4 +1,4 @@
- document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.15.1"; 
+ document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.15.2"; 
 
 var circle1 = document.getElementById("id_circle1");
 var circle2 = document.getElementById("id_circle2");
@@ -17,8 +17,14 @@ function on_touch_move(e)
 	var touches = e.changedTouches;
 	for (var i = 0; i < touches.length; i++){
 		var circle = touches[i].target;
-		circle.setAttribute("cx", touches[i].pageX - rect_svg.left);
-		circle.setAttribute("cy", touches[i].pageY - rect_svg.top);
+		if (touches[i].pageX - rect_svg.left <= rect_svg.width && 
+			touches[i].pageY - rect_svg.top <= rect_svg.height &&
+			touches[i].pageX - rect_svg.left >= 0 &&
+			touches[i].pageY - rect_svg.top >= 0
+			){
+			circle.setAttribute("cx", touches[i].pageX - rect_svg.left);
+			circle.setAttribute("cy", touches[i].pageY - rect_svg.top);
+		}
 	}
 }
 //----------------------------------------
