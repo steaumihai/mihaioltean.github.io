f0c56358038edab0e3fdcc68d27c0f3993130a0e
mihaioltean <mihai.oltean@gmail.com>
2017-11-15 18:09:08 +0200

    update svg version number

--- js/svg_touch.js
+++ js/svg_touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.15.2"; 
+document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.15.3"; 
 
 addEventListener("touchstart", on_touch_start);
 
@@ -6,7 +6,7 @@ function on_touch_start(e)
 {
 	var touches = e.changedTouches;
 	for (var i = 0; i < touches.length; i++){
-		var circle = document.createElementNS("http://www.w3.org/2000/svg2", "circle");
+		var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 		circle.setAttribute("cx", touches[i].pageX);
 		circle.setAttribute("cy", touches[i].pageY);
 		circle.setAttribute("r", 10);
