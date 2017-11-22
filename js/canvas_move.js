59c6212e2b07d517d55bf9403290458b06a8e814
mihaioltean <mihai.oltean@gmail.com>
2017-11-22 17:54:57 +0200

    fix clearRect

--- js/canvas_move.js
+++ js/canvas_move.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.22.1"; 
+document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.22.2"; 
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
@@ -27,7 +27,7 @@ function on_touch_move(e)
 	for (var i = 0; i < touches.length; i++){
 		if (touches[i].pageX - rect_canvas.left < top_x + img_width && touches[i].pageX - rect_canvas.left >= top_x && 
 			touches[i].pageY - rect_canvas.top < top_y + img_height && touches[i].pageY - rect_canvas.top >= top_y) {
-				context.clearRect();
+				context.clearRect(0, 0, 400, 300);
 				top_x = touches[i].pageX - rect_canvas.left;
 				top_y = touches[i].pageY - rect_canvas.top;
 				context.drawImage(img, top_x, top_y, img_width, img_height);
