d3507ed4beb84f5cffebae9789e6627a4640adbe
mihaioltean <mihai.oltean@gmail.com>
2017-11-22 19:41:55 +0200

    added select

--- js/synt.js
+++ js/synt.js
@@ -1,4 +1,4 @@
- document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.22.3"; 
+ document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.22.5"; 
  
  var synt = window.speechSynthesis;
  
@@ -6,8 +6,11 @@
  function get_voices()
  {
 	 var voices = synt.getVoices();
-	 for (var i = 0; i < voices.length; i++)
-		 document.getElementById ("id_voices").innerHTML += voices[i].name + ":" + voices[i].lang + "<br>";
+	 for (var i = 0; i < voices.length; i++){
+		 var e = document.createElement("option");
+		 e.text = voices[i].lang;
+		 document.getElementById ("id_voices").add(e);
+	 }
  }
  //---------------------------------------
  function speak()
@@ -15,6 +18,16 @@
 	var enunt = new SpeechSynthesisUtterance();
 	enunt.lang = "it-IT";
 	enunt.text = document.getElementById("id_text").value;
+	
+	enunt.onerror = function(e){
+		alert(e.error);
+	}
+	
+	enunt.onend = function(e){
+		document.getElementById("id_button_speak").disabled = false;
+	}
+	
+	document.getElementById("id_button_speak").disabled = true;
 	synt.speak(enunt);	
  }
   //---------------------------------------
