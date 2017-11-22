9ed26e2d753534fcb328591e8588c8e13d0f0d30
mihaioltean <mihai.oltean@gmail.com>
2017-11-22 18:09:02 +0200

    better position

--- js/canvas_move.js
+++ js/canvas_move.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.22.3"; 
+document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.22.4"; 
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
@@ -27,8 +27,8 @@ function on_touch_move(e)
 	for (var i = 0; i < touches.length; i++){
 		if (touches[i].pageX - rect_canvas.left < top_x + img_width && touches[i].pageX - rect_canvas.left >= top_x && 
 			touches[i].pageY - rect_canvas.top < top_y + img_height && touches[i].pageY - rect_canvas.top >= top_y) {
-				var offset_inside_image_x = top_x - (touches[i].pageX - rect_canvas.left);
-				var offset_inside_image_y = top_y - (touches[i].pageY - rect_canvas.top);
+				var offset_inside_image_x = (touches[i].pageX - rect_canvas.left) - top_x;
+				var offset_inside_image_y = (touches[i].pageY - rect_canvas.top) - top_y;
 				context.clearRect(0, 0, 400, 300);
 				top_x = touches[i].pageX - rect_canvas.left - offset_inside_image_x;
 				top_y = touches[i].pageY - rect_canvas.top - offset_inside_image_y;
