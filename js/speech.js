85ea1844bc1bd41e833478db2c4ef572308f41f8
mihaioltean <mihai.oltean@gmail.com>
2017-11-29 19:20:56 +0200

    alternatives

--- js/speech.js
+++ js/speech.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_level_version").innerHTML = "Business level version: 2017.11.29.4"; 
+document.getElementById("id_business_level_version").innerHTML = "Business level version: 2017.11.29.5"; 
 
 document.addEventListener("touchstart", on_touch_start);
 
@@ -22,7 +22,7 @@ function on_speech_result(e)
 {
 	var alternatives = e.results[0];
 	for (var i = 0; i < alternatives.length; i++)
-	document.getElementById("id_p").innerHTML += alternatives[i].transcript + "(" + alternatives.confidence + ")" + "<br>";
+	document.getElementById("id_p").innerHTML += alternatives[i].transcript + "(" + alternatives[i].confidence + ")" + "<br>";
 }
 //-----------------------------------------
 function on_sound_end(e)
