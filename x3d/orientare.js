4283fe4a7726644fa61247672b63146468e5940d
mihaioltean <mihai.oltean@gmail.com>
2018-03-16 18:28:12 +0200

    orientare

---
+++ x3d/orientare.js
@@ -0,0 +1,6 @@
+window.addEventListener("deviceorientation", on_device_orientation);
+
+function on_device_orientation(e)
+{
+	document.getElementById("id_orientation").innerHTML = e.alpha + " " + e.beta + " " + e.gamma;
+}
