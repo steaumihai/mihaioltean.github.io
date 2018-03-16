6dc30fe2664b98fd848c29f6395a4a0c50b64a7f
mihaioltean <mihai.oltean@gmail.com>
2018-03-16 18:33:44 +0200

    sterge cifre

--- x3d/orientare.js
+++ x3d/orientare.js
@@ -2,5 +2,5 @@ window.addEventListener("deviceorientation", on_device_orientation);
 
 function on_device_orientation(e)
 {
-	document.getElementById("id_orientation").innerHTML = e.alpha + " " + e.beta + " " + e.gamma;
+	document.getElementById("id_orientation").innerHTML = e.alpha.toFixed(1) + " " + e.beta.toFixed(1) + " " + e.gamma.toFixed(1);
 }
