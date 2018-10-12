5a781dfa910de6dbb53eb41696a6b41243c2a51d
Mihai Oltean <mihai.oltean@gmail.com>
2018-10-12 18:38:55 +0300

    solve equation

--- js/ec2.js
+++ js/ec2.js
@@ -1,1 +1,28 @@
-document.getElementById("id_business_version").innerHTML = "Bussiness version: 2018.10.12.0";
+document.getElementById("id_business_version").innerHTML = "Bussiness version: 2018.10.12.1";
+
+function solve()
+{
+	var a = document.getElementById("id_a").value;
+	var b = document.getElementById("id_b").value;
+	var c = document.getElementById("id_c").value;
+	
+	
+	var delta = b * b - 4 * a * c;
+	
+	var x1_re, x1_im, x2_re, x2_im;
+	if (delta >= 0){
+		x1_re = (-b - Math.sqrt(delta)) / (2 * a);
+		x1_im = 0;
+		x2_re = (-b + Math.sqrt(delta)) / (2 * a);
+		x2_im = 0;
+	}
+	else{
+		x1_re = -b / (2 * a);
+		x1_im = -Math.sqrt(-delta) / (2 * a);
+		x2_re = -b / (2 * a);
+		x2_im = Math.sqrt(-delta) / (2 * a);
+	}
+	
+	document.getElementById("id_x1").innerHTML = x1_re + " + " + x1_im + "i";
+	document.getElementById("id_x1").innerHTML = x2_re + " + " + x2_im + "i";
+}
