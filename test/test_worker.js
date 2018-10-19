7da08879ce8f1b2e58423c5c5b580a0ad70fa6a2
Mihai Oltean <mihai.oltean@gmail.com>
2018-10-19 12:51:11 +0300

    Update test/test_worker.js

--- test/test_worker.js
+++ test/test_worker.js
@@ -3,16 +3,16 @@ function start_worker()
 {
 	if (window.Worker) { // Check if Browser supports the Worker api.
 	// Requires script name as input
-		var myWorker = new Worker("worker.js");
+		//var myWorker = new Worker("worker.js");
 
 		for (var i = 1e7; i < 1e9; i++)
 			if (is_prime(i)){
 				document.getElementById("id_main").innerHTML += i + " ";
 		}
 
-		myWorker.onmessage = function(e) {
-			document.getElementById("id_worker").innerHTML += e.data + " ";
-		};
+		//myWorker.onmessage = function(e) {
+		//	document.getElementById("id_worker").innerHTML += e.data + " ";
+		//};
 	}
 }
 //-------------------------------------------------
