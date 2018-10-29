a09d86caa46fddda6388e4593b75af7dc5f5d603
Mihai Oltean <mihai.oltean@gmail.com>
2018-10-29 15:07:23 +0200

    start added

--- html5-2/calcul_prime.js
+++ html5-2/calcul_prime.js
@@ -1,9 +1,14 @@
 var stopped = false;
 
+//------------------------------ 
 onmessage = function(e){
 	if (e.data == "stop"){
 		stopped = true;
 	}
+	else
+		if (e.data == "start"){
+			stopped = false;
+		}
 }
 //------------------------------ 
 function is_prim(n)
--- html5-2/desenare.js
+++ html5-2/desenare.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.29.3";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.29.4";
 document.getElementById("id_start_button").addEventListener("click", start);
 document.getElementById("id_stop_button").addEventListener("click", stop);
 
@@ -6,7 +6,7 @@ document.getElementById("id_start_button").disabled = false;
 document.getElementById("id_stop_button").disabled = true;
 
 var unghi = {valoare:0};
-	
+var my_worker = null;
 //-----------------------------------
 function deseneaza_cerc(context, w, h, unghi)
 {
@@ -30,10 +30,14 @@ function start()
 	document.getElementById("id_start_button").disabled = true;
 	document.getElementById("id_stop_button").disabled = false;
 	
-	my_worker = new Worker("calcul_prime.js");
-	my_worker.onmessage = function(e){
-		document.getElementById("id_prime").innerHTML = e.data;
+	if (my_worker == null){
+		my_worker = new Worker("calcul_prime.js");
+		my_worker.onmessage = function(e){
+			document.getElementById("id_prime").innerHTML = e.data;
+		}
 	}
+	else
+		my_worker.postMessage("start");
 	
 	id_timer = setInterval(deseneaza_cerc, 10, context, canvas.width, canvas.height, unghi);
 }
