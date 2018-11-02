3f038c64bfd05c78f2687ebe53a0fa6099429e99
Mihai Oltean <mihai.oltean@gmail.com>
2018-11-02 18:59:20 +0200

    canvas

--- js/orientare.js
+++ js/orientare.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Bussiness version: 2018.11.02.1";
+document.getElementById("id_business_version").innerHTML = "Bussiness version: 2018.11.02.2";
 window.addEventListener("deviceorientation", on_device_orientation);
 
 function on_device_orientation(e)
@@ -6,4 +6,16 @@ function on_device_orientation(e)
 	document.getElementById("id_z").innerHTML = e.alpha;
 	document.getElementById("id_x").innerHTML = e.beta;
 	document.getElementById("id_y").innerHTML = e.gamma;
+	
+	var canvas = document.getElementById("id_canvas");
+	var context = canvas.getContext("2d");
+	
+	var R = 10;
+	
+	context.beginPath();
+	context.arc(canvas.width / 2 + e.beta / 90 * (canvas.width / 2 - R),
+				canvas.height / 2 + e.gamma / 90 * (canvas.height / 2 - R),	
+				R,
+				0, 2 * Math.PI);
+	context.stroke();
 }
