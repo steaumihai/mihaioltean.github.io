567adeb606f1a277cebb4b869ac0b829fa035f28
Mihai Oltean <mihai.oltean@gmail.com>
2018-11-02 18:25:21 +0200

    Update js/orientare.js

--- js/orientare.js
+++ js/orientare.js
@@ -1,9 +1,9 @@
-document.getElementById("id_business_version").innerHTML = "Bussiness version: 2018.11.02.0";
+document.getElementById("id_business_version").innerHTML = "Bussiness version: 2018.11.02.1";
 window.addEventListener("deviceorientation", on_device_orientation);
 
 function on_device_orientation(e)
 {
 	document.getElementById("id_z").innerHTML = e.alpha;
-	document.getElementById("id_x").innerHTML = e.gamma;
-	document.getElementById("id_y").innerHTML = e.beta;
+	document.getElementById("id_x").innerHTML = e.beta;
+	document.getElementById("id_y").innerHTML = e.gamma;
 }
